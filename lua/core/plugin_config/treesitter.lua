08efac6b5b9e68b047715b9de46ba7c6e9206168
tekiyami <tekiyami@zohomail.com>
2023-04-14 13:28:26 +0800

    first config

---
+++ lua/core/plugin_config/treesitter.lua
@@ -0,0 +1,7 @@
+require 'nvim-treesitter.configs'.setup {
+  ensure_installed = { "c", "lua", "rust", "verilog" },
+
+  sync_install = false,
+  auto_install = true,
+  highlight = { enable = true, },
+}
